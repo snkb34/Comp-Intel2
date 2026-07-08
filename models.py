0f8eb4c5fcdfb3bd988cbb7a695ba080b2585aaa
snkb34 <barnes10@mac.com>
2026-07-08 14:13:47 -0600

    Delete models.py

--- models.py
+++
@@ -1,3 +0,0 @@
-{% extends "base.html" %}{% block content %}
-<div class="card"><h3>Downloaded Documents</h3><table><tr><th>Status</th><th>Size</th><th>Content Type</th><th>Final URL</th><th>Message</th><th>Downloaded</th></tr>{% for d in docs %}<tr><td>{{d.status}}</td><td>{{d.file_size}}</td><td>{{d.content_type}}</td><td><a href="{{d.final_url}}" target="_blank">{{d.final_url}}</a></td><td>{{d.message}}</td><td>{{d.downloaded_at}}</td></tr>{% endfor %}</table></div>
-{% endblock %}
